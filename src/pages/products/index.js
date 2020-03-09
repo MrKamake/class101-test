6da5e44ede1ee8bc0d2a34c0b5698c5101c2ca9d
MrKamake <seongjooon@gmail.com>
2020-03-09 12:49:27 +0900

    Rendered Products page

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { getDataApi } from '../../utils';
+import ProductItem from '../../components/ProductItem';
 
 const Products = () => {
   const [productItems, setProductItems] = useState([]);
 
   useEffect(() => {
     getDataApi('productItems').then(res => {
+      res.sort((a, b) => b.score - a.score);
       setProductItems(res);
     });
   }, []);
 
-  return <div className="Products">Products Page</div>;
+  const renderProducts = () =>
+    productItems.map(product => <ProductItem key={product.id} {...product} />);
+
+  return (
+    <div className="Products">
+      {productItems.length === 0 ? <h1>Loading...</h1> : renderProducts()}
+    </div>
+  );
 };
 
 export default Products;
